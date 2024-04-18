42fdd4f74818d96dd985de77dd163c10d267592f
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-18 10:36:12 +0530

    update logic

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -2,18 +2,11 @@ export default function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
 
-    if(num1 === num2){
-        return (
-            <div>
-                <h1>You Win!!</h1>
-                <p>Num1: {num1}</p>
-                <p>Num2: {num2}</p>
-            </div>
-        );
-    }
+    const result = num1 === num2 ? "You Win!!" : "You Lose:(";
+    
     return (
         <div>
-            <h1>You Lose:(</h1>
+            <h1>{result}</h1>
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
