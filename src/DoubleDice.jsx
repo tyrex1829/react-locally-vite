05d2335b720277455255d86850ec6ce5e7bf26c2
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-18 10:37:12 +0530

    update logic - inline ternary

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,12 +1,10 @@
 export default function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
-
-    const result = num1 === num2 ? "You Win!!" : "You Lose:(";
     
     return (
         <div>
-            <h1>{result}</h1>
+            <h1>{num1 === num2 ? "You Win!!" : "You Lose:("}</h1>
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
