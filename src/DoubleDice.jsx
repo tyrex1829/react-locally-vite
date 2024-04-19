254948ecccfe48ed92bff57aaef9b1be656a84f4
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-19 09:37:16 +0530

    ternary to AND logic

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,16 +1,38 @@
 export default function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
-    
+        
     return (
         <div>
             <h1>Double Dice</h1>
-            {num1 === num2 ? <h3>You Win!!</h3> : null}
+            {num1 === num2 && <h3>You Win!!</h3>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
     );
 }
+
+
+
+
+
+
+
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor(Math.random() * 3) + 1;
+//     const num2 = Math.floor(Math.random() * 3) + 1;
+    
+//     return (
+//         <div>
+//             <h1>Double Dice</h1>
+//             {num1 === num2 ? <h3>You Win!!</h3> : null}
+//             <p>Num1: {num1}</p>
+//             <p>Num2: {num2}</p>
+//         </div>
+//     );
+// }
 
 
 
