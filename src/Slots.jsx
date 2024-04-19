94b101951ebc7a5e85c74f3eb73d9975b9dd6a16
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-19 11:57:47 +0530

    cleaned code

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,14 +1,41 @@
 export default function Slots ({ val1, val2, val3 }) {
+
+    const isWinner = val1 === val2 && val1 === val3;
     
     const loseStyle = { color: "red" };
     const winStyle = { color: "green" };
-
+    
     return (
         <div>
             <h1>
                 {val1}{val2}{val3}
             </h1>
-            { (val1 === val2 && val1 === val3) ? <><p style={ winStyle }>You Win</p><p>congrats</p></> : <p style={ loseStyle }>You lose</p> }
+            <h2>
+                { isWinner ? "You Win!" : "You lose" }
+            </h2>
+            { isWinner && <h3>Congrats</h3> }
         </div>
-    )
+    );
 }
+
+
+
+
+
+
+
+
+// export default function Slots ({ val1, val2, val3 }) {
+    
+//     const loseStyle = { color: "red" };
+//     const winStyle = { color: "green" };
+
+//     return (
+//         <div>
+//             <h1>
+//                 {val1}{val2}{val3}
+//             </h1>
+//             { (val1 === val2 && val1 === val3) ? <><p style={ winStyle }>You Win</p><p>congrats</p></> : <p style={ loseStyle }>You lose</p> }
+//         </div>
+//     )
+// }
