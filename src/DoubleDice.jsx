3a9ba5b65d78dcce326b1ba619b3ab151b65fe33
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-19 09:28:48 +0530

    inline ternary operator

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -9,5 +9,23 @@ export default function DoubleDice() {
             <p>Num2: {num2}</p>
         </div>
     );
-    
 }
+
+
+
+
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor(Math.random() * 3) + 1;
+//     const num2 = Math.floor(Math.random() * 3) + 1;
+    
+//     return (
+//         <div>
+//             <h1>{num1 === num2 ? "You Win!!" : "You Lose:("}</h1>
+//             <p>Num1: {num1}</p>
+//             <p>Num2: {num2}</p>
+//         </div>
+//     );
+    
+// }
