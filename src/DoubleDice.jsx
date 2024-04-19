50fc4701e1bff665184176f76bf2039daef50a85
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-19 09:58:04 +0530

    dynamic style

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,11 +1,17 @@
 export default function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
-        
+
+    const isWinner = num1 === num2;
+    
+    const divStyles = { backgroundColor: "#000", padding: "30px", marginBottom: "20px", borderRadius: "20px", color: isWinner ? "green" : "red" };
+
+    const h1Styles = { color: "purple" };
+
     return (
-        <div>
-            <h1>Double Dice</h1>
-            {num1 === num2 && <h3>You Win!!</h3>}
+        <div className="DoubleDice" style={divStyles}>
+            <h1 style={h1Styles}>Double Dice</h1>
+            {isWinner && <h3>You Win!!</h3>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
