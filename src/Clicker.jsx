34cf4340a3888de90065a4e66b9304f548dad8ea
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-19 21:42:09 +0530

    react more events

--- src/Clicker.jsx
+++ src/Clicker.jsx
@@ -2,10 +2,14 @@ function handleClick () {
     console.log("CLicked The Button!!!")
 }
 
+function handleHover () {
+    console.log("HOVERED!!")
+}
+
 export default function Clicker () {
     return (
         <div>
-            <p>Click the button</p>
+            <p onMouseOver={handleHover}>Hover me</p>
             <button onClick={handleClick}>Click</button>
         </div>
     );
