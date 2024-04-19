2e0d71d52faf11f19863a977f157f9ff72b0a75c
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-19 16:48:42 +0530

    spread operator

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -4,7 +4,7 @@ function ShoppingList ({ items }) {
     return (
         <ul>
             { items.map((i) => (
-                <ShoppingListItem key={i.id} item={i.item} quantity={i.quantity} completed={i.completed} />
+                <ShoppingListItem key={i.id} {...i} />
             )) }
         </ul>
     );
