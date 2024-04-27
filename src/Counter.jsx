0026c3723e269b1065b0e9a89d9e880f57426420
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-27 10:17:11 +0530

    console counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 
 function Counter () {
     let [count, setCount] = useState(0);
+    console.log("Component is rerendered");
+    console.log(count);
     return (
         <div>
             <p>count is: {count}</p>
             <button onClick={() => {
-                setCount(count + 1)
+                setCount(count + 1);
+                console.log("value changes");
+                console.log(count);
             }}>Increment</button>
         </div>
     )
